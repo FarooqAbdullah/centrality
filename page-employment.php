0de416bdd5d6f403d642bc32b14cdaf767c095c4
sajiddesigner <sajidraja1@yahoo.com>
2015-07-09 12:14:57 +0500

    Added New Innepage.css file & page-emplyment.php

---
+++ page-employment.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying Home Page Content
+ *
+ * @package Objects
+ * @subpackage Centrality
+ */
+
+/* Template Name: Employment-Page */
+    get_header();the_post();
+?>
+
+<div class="header-wrapper employment-header"></div>
+<div class="content-wrapper">
+    <div class="row">
+         <div class="container">
+            
+        </div>
+    </div>
+    
+    
+       
+    </div>
+
+
+<?php get_footer(); ?>
