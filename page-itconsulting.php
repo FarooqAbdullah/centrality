db9adbe9471cab39fc7252cccfae7e13c065cf19
sajiddesigner <sajidraja1@yahoo.com>
2015-07-14 15:22:02 +0500

    IT COnsulting page started

---
+++ page-itconsulting.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * The IT Consulting Page Template
+ *
+ * @package Objects
+ * @subpackage Centrality
+ */
+
+/* Template Name: IT Consulting Page */
+get_header();the_post();
+?>
+    <div class="content-wrapper it-consulting-page-heading">
+	<div class="container">
+	<div class="breadcrumbs">  Solutions / IT Management - IT Consulting</div>
+	<?php $pfx="solutions_"; ?>
+
+<h2><?php echo the_title();  ?> </h2>
+</div>
+</div>
+<div class="content-wrapper centralityServices">
+  <?php echo do_shortcode('[_centrality_service]'); ?>
+  
+</div>
+<div class="content-wrapper cabling-solutions">
+<?php $hd_img=get_post_meta( get_the_ID(), $pfx.'cab_heading', true );
+$hd_slogan=get_post_meta( get_the_ID(), $pfx.'cab_text', true );
+$btntxt=get_post_meta( get_the_ID(), $pfx.'btn', true );
+$btnurl=get_post_meta( get_the_ID(), $pfx.'btnUrl', true );
+$cab_icon=get_post_meta( get_the_ID(), $pfx.'cab_icon', true );
+
+ echo do_shortcode('[_employment_header class="employment-header" head_img="'.$cab_icon.'" heading="'.$hd_img.'" slogan="'.$hd_slogan.'" button_text="'.$btntxt.'" button_url="'.btnurl.'"]'); ?>
+ </div>
+
+ <div class="content-wrapper featured-sol greybgwrap">
+    <div class="blueBgwrap">
+	 <h2>Featured Solutions</h2>
+	 <div class="container">
+	 <div class="row-minus">
+		 <div class="col-lg-3">
+			<div class="featuredServiceBox">
+			<img src="<?php echo get_template_directory_uri() ?>/images/cloudicon.jpg" /> 
+			<h2>Managed Cloud Services</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+			<a href="#" class="btn-square blue-border">LEARN MORE</a>
+			<div class="clear-fix texture1 texture"><img src="<?php echo get_template_directory_uri() ?>/images/texture1.jpg" /> </div>
+			</div>
+		 </div>
+		 <!--Col-->
+		 
+		 <div class="col-lg-3">
+			<div class="featuredServiceBox">
+			<img src="<?php echo get_template_directory_uri() ?>/images/bandaicon.jpg" /> 
+			<h2>On Premise IT Support</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+			<a href="#" class="btn-square blue-border">LEARN MORE</a>
+			<div class="clear-fix texture1 texture"><img src="<?php echo get_template_directory_uri() ?>/images/texture1.jpg" /> </div>
+			</div>
+		 </div>
+		 <!--Col-->
+		 
+		 <div class="col-lg-3">
+			<div class="featuredServiceBox">
+			<img src="<?php echo get_template_directory_uri() ?>/images/phoneicon.jpg" /> 
+			<h2>Voice Communications</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+			<a href="#" class="btn-square blue-border">LEARN MORE</a>
+			<div class="clear-fix texture1 texture"><img src="<?php echo get_template_directory_uri() ?>/images/texture1.jpg" /> </div>
+			</div>
+		 </div>
+		 <!--Col-->
+		 
+		 <div class="col-lg-3">
+			<div class="featuredServiceBox last">
+			<img src="<?php echo get_template_directory_uri() ?>/images/plusicon.jpg" /> 
+			<h2>Business Continuity/ Disaster Recovery</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+			<a href="#" class="btn-square blue-border">LEARN MORE</a>
+			<div class="clear-fix texture1 texture"><img src="<?php echo get_template_directory_uri() ?>/images/texture1.jpg" /> </div>
+			</div>
+		 </div>
+		 <!--Col-->
+	</div>
+		 
+		 <div class="clear-fix"></div>
+	</div>
+	</div>
+ </div>
+
+
+    
+
+
+
+
+
+
+
+<div class="content-wrapper"><?php echo do_shortcode('[_trusted_companies]'); ?></div>
+<?php get_footer(); ?>
