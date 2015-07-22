9787e8b95c726ac3c3050dba0d01ec8c53f0e0f6
FarooqAbdullah <farooq14162@gmail.com>
2015-07-22 19:16:14 +0500

    remove steps from our story page

--- page-ourstory.php
+++ page-ourstory.php
@@ -57,39 +57,6 @@ $box4Text = get_post_meta( get_the_ID(), 'why_centrality_prBox4_title', true );
 $btnText = get_post_meta( get_the_ID(), 'why_centrality_discoverbtn', true );
 $btnTextUrl = get_post_meta( get_the_ID(), 'why_centrality_discoverbtnurl', true );
 ?>
-    <div class="row proven-process">
-        <div class="container">
-            <h2> <?php echo $heading; ?> </h2>
-
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 cSection">
-                <div class="number circle"> 1 <span class="steps-line line-right"></span> </div>
-                <h3><?php echo $box1Heading; ?></h3>
-                <p><?php echo $box1Text; ?></p>
-            </div>
-
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 cSection">
-                <div class="number circle"> <span class="steps-line line-left"></span> 2 <span class="steps-line line-right"></span> </div>
-                <h3><?php echo $box2Heading; ?></h3>
-                <p><?php echo $box2Text; ?></p>
-            </div>
-
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 cSection">
-                <div class="number circle"><span class="steps-line line-left"></span> 3<span class="steps-line line-right"></span> </div>
-                <h3><?php echo $box3Heading; ?></h3>
-                <p><?php echo $box3Text; ?></p>
-            </div>
-
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 cSection">
-                <div class="number circle"><span class="steps-line line-left"></span> 4 </div>
-                <h3><?php echo $box4Heading; ?></h3>
-                <p><?php echo $box4Text; ?></p>
-            </div>
-
-            <div class="clearfix"></div>
-            <a href="<?php echo $btnTextUrl; ?>" class="discover-more"><?php echo $btnText; ?></a>
-
-        </div>
-    </div>
 
 <?php echo do_shortcode('[_trusted_companies]'); ?>
 </div>
